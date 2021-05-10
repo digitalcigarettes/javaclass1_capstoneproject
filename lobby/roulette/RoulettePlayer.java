76a76dea2a9c0ffcf3655487f310f2a44f568afc
yourboirushil <mlgerrydoge@gmail.com>
2021-05-10 13:36:17 -0700

    fix

--- lobby/roulette/RoulettePlayer.java
+++ lobby/roulette/RoulettePlayer.java
@@ -2,7 +2,7 @@
 public class RoulettePlayer {
 
 	public static void main(String[] args) {
-		RouletteWheel theEpicWheel = new RouletteWheel(0); //tester class, will not be necessary in the future
+		RouletteWheel theEpicWheel = new RouletteWheel(100000); //tester class, will not be necessary in the future
 	}
 
 }
