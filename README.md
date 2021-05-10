c50f32582b5c51051b4a25ccdcc614d34c948f95
yourboirushil <mlgerrydoge@gmail.com>
2021-05-10 13:35:19 -0700

    added finished roulette wheel
    yes

--- README.md
+++
@@ -1,4 +0,0 @@
-# capstoneproject
-final project for java class
-The traditional README.md
-pls note this is Not the readme.pdf that is required for the project
--- lobby/roulette/RoulettePlayer.java
+++ lobby/roulette/RoulettePlayer.java
@@ -2,7 +2,7 @@
 public class RoulettePlayer {
 
 	public static void main(String[] args) {
-		RouletteWheel theEpicWheel = new RouletteWheel(0);
+		RouletteWheel theEpicWheel = new RouletteWheel(0); //tester class, will not be necessary in the future
 	}
 
 }
