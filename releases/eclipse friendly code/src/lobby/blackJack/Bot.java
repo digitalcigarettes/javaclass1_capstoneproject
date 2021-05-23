42ee4dad5ec14c7f7293a682984942619c98e6c0
Edwarzzz <edwardlu.042@gmail.com>
2021-05-23 16:46:14 -0700

    fin

---
+++ releases/eclipse friendly code/src/lobby/blackJack/Bot.java
@@ -0,0 +1,45 @@
+package lobby.blackJack;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.Random;
+
+public class Bot {
+    String username;
+    int draws, bank;
+    public List<Integer> hand = new ArrayList<Integer>();
+
+
+    public Bot(String username, int bank){
+        this.username = username;
+        this.draws = 0;
+        this.bank = bank;
+    }
+
+    public boolean drawAgain(int sum){
+    	if(sum==0){
+            return false;
+        }else if(sum < 17){
+            return true;
+        }
+        return false;
+    }
+
+    public void hit(int card){
+        this.hand.add(card);
+        this.draws++;
+    }
+
+    public void win(int money){
+        this.bank+=money;
+    }
+
+    public void loss(int money){
+        this.bank -= money;
+    }
+    public void refresh(){
+        this.hand.removeAll(this.hand);
+    }
+
+
+}
